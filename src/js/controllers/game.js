b62072d1a236a21fcee98079fef48c96f1e71654
Arkaitz Garro <arkaitz.garro@gmail.com>
2018-10-14 16:30:14 +0200

    Implement basic game rules

--- src/js/controllers/game.js
+++ src/js/controllers/game.js
@@ -3,9 +3,62 @@ import { Controller } from 'stimulus';
 import games from '../games/definition';
 
 export default class extends Controller {
-  game = null;
+  static targets = [ 'scoreBlue', 'scoreRed' ]
 
   connect() {
-    console.log("Hello, Stimulus!", this.element)
+    console.log('Ready to play?', this);
+    this.init();
+  }
+
+  init() {
+    this.game = null;
+    this.serve = null;
+    this.score = {
+      blue: 0,
+      red: 0
+    }
+  }
+
+  selectGame(event) {
+    this.game = games[event.target.dataset.game];
+  }
+
+  updateScore(event) {
+    const player = event.target.dataset.player;
+
+    // Only update score is someone is already serving
+    if (this.serve) {
+      this.score[player]++;
+    }
+
+    this.determineServe(player);
+    this.updateView();
+  }
+
+  updateView() {
+    // Update the scores
+    this.scoreBlueTarget.innerHTML = this.score.blue;
+    this.scoreRedTarget.innerHTML = this.score.red;
+
+    // Show the paddle for the player serving
+    this.element.classList.toggle('serve--blue', this.serve === 'blue');
+    this.element.classList.toggle('serve--red', this.serve === 'red');
+  }
+
+  determineServe(player) {
+    if (!this.serve) {
+      this.serve = player;
+    } else {
+      const totalScore = this.score.blue + this.score.red;
+      // Switch players depending on the game
+      if (totalScore % this.game.switchServe === 0) {
+        this.serve = this.serve === 'blue' ? 'red' : 'blue';
+      }
+    }
+  }
+
+  reset() {
+    this.init();
+    this.updateView();
   }
 }
--- src/js/games/definition.js
+++ src/js/games/definition.js
@@ -1,4 +1,8 @@
 export default {
+  free: {
+    points: Infinity,
+    switchServe: Infinity
+  },
   short: {
     points: 11,
     switchServe: 2
